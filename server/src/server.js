cded1d3482ff3da8b112630bfffd9d7591ff97ce
AbSaq <abdulrahman.salem.akeel@gmail.com>
2026-03-06 15:16:24 +0300

    chore(server): create barebone server and setup husky

--- server/src/server.js
+++ server/src/server.js
@@ -2,8 +2,12 @@ const express = require("express")
 
 const app = express()
 
+const PORT = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
   res.send("API running")
 })
 
-app.listen(3000)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
